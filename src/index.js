3b40b4e87a2311e370806a6371a0d7a4faa3ea22
LaksCastro <lakhsmycastro336@gmail.com>
2020-03-19 21:40:29 -0300

    remove pure script and add dom loaded event listener to run this script

--- src/index.js
+++ src/index.js
@@ -4,45 +4,49 @@ import MusicPlayer from "./scripts/player"
 import keyboard from "./scripts/shortcuts"
 import songs from "./scripts/data"
 
-const player = MusicPlayer({
-  playButton: "#play",
-  nextButton: "#next",
-  prevButton: "#prev",
-  muteButton: "#toggle-mute",
-  loopButton: "#loop",
-  randomButton: "#random",
-  timerDisplay: "#timer",
-  volRange: "#vol",
-  durationRange: "#duration",
-  songs,
-});
+document.addEventListener('DOMContentLoaded', init);
 
-player.onSongChange = (updatedPlayer) => {
-  console.log(updatedPlayer);
-  document.querySelector("#content").innerHTML = `
-  <h1>${updatedPlayer.songs[updatedPlayer.currentSong].name}</h1>
-  <p>${updatedPlayer.currentSong}</p>
-  <img width="450" src="${updatedPlayer.songs[updatedPlayer.currentSong].coverSrc}" alt="${updatedPlayer.songs[updatedPlayer.currentSong].name}" />
-  `
+function init() {
+  const player = MusicPlayer({
+    playButton: "#play",
+    nextButton: "#next",
+    prevButton: "#prev",
+    muteButton: "#toggle-mute",
+    loopButton: "#loop",
+    randomButton: "#random",
+    timerDisplay: "#timer",
+    volRange: "#vol",
+    durationRange: "#duration",
+    songs,
+  });
+
+  player.onSongChange = (updatedPlayer) => {
+    console.log(updatedPlayer);
+    document.querySelector("#content").innerHTML = `
+    <h1>${updatedPlayer.songs[updatedPlayer.currentSong].name}</h1>
+    <p>${updatedPlayer.currentSong}</p>
+    <img width="450" src="${updatedPlayer.songs[updatedPlayer.currentSong].coverSrc}" alt="${updatedPlayer.songs[updatedPlayer.currentSong].name}" />
+    `
+  }
+  player.init();
+
+  keyboard.on([{
+    key: 32,
+    callback: player.togglePlaying.bind(player)
+  }, {
+    key: 39,
+    callback: player.next.bind(player)
+  }, {
+    key: 37,
+    callback: player.prev.bind(player)
+  }, {
+    key: 77,
+    callback: player.toggleMute.bind(player)
+  }, {
+    key: 82,
+    callback: player.toggleRandomMode.bind(player)
+  }, {
+    key: 76,
+    callback: player.toggleLoopMode.bind(player)
+  }]);
 }
-player.init();
-
-keyboard.on([{
-  key: 32,
-  callback: player.togglePlaying.bind(player)
-}, {
-  key: 39,
-  callback: player.next.bind(player)
-}, {
-  key: 37,
-  callback: player.prev.bind(player)
-}, {
-  key: 77,
-  callback: player.toggleMute.bind(player)
-}, {
-  key: 82,
-  callback: player.toggleRandomMode.bind(player)
-}, {
-  key: 76,
-  callback: player.toggleLoopMode.bind(player)
-}]);
