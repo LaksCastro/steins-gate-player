2e8c6858690e33a9d2e0c1011b62af49668b84c6
LaksCastro <lakhsmycastro336@gmail.com>
2020-03-22 21:58:07 -0300

    added palette animation to card song animatioN

--- src/scripts/simple-animations.js
+++ src/scripts/simple-animations.js
@@ -8,15 +8,30 @@ function simpleAnimations() {
         const style = card.style;
 
         let animationId = null;
+        let paletteId = null;
         let allowAnimate = false;
 
         card.onmouseenter = function () {
             allowAnimate = true;
+            changePalette();
             animate();
         }
         card.onmouseleave = function () {
             allowAnimate = false;
             clearTimeout(animationId);
+            clearTimeout(paletteId);
+        }
+
+        let palette = generateRandomColorPalette();
+
+
+        function changePalette() {
+            if (!allowAnimate) return;
+
+            paletteId = setTimeout(() => {
+                palette = generateRandomColorPalette();
+                changePalette();
+            }, 6000);
         }
 
         function animate() {
@@ -24,10 +39,10 @@ function simpleAnimations() {
 
             animationId = setTimeout(() => {
                 style.setProperty('--after-height', `${randomIntFromInterval(0, 30)}px`);
-                style.setProperty('--after-background', `${generateRandomColor().string}`);
+                style.setProperty('--after-background', `${generateRandomColor(palette).string}`);
 
                 style.setProperty('--before-height', `${randomIntFromInterval(0, 30)}px`);
-                style.setProperty('--before-background', `${generateRandomColor().string}`);
+                style.setProperty('--before-background', `${generateRandomColor(palette).string}`);
 
                 animate();
             }, 100);
